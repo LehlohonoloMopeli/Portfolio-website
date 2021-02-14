c0cbcf23a451b1d9ffa5209a27da9a183fba415f
LehlohonoloMopeli <lehlohonolompl@gmail.com>
2021-02-14 13:36:45 +0200

    Deleted unused photos

--- src/App/App.js
+++ src/App/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import Header from '../Header/Header'
-import Home from '../Home/Home' 
-import About from '../About/About'
+import Header from '../Header/Header';
+import Home from '../Home/Home';
+import About from '../About/About';
+import Skills from '../Skills/Skills'
 import './App.css';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <Home />
       <About />
+      <Skills />
     </div>
   );
 }
---
+++ src/Skills/Skills.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Skills.css'
+
+function Skills() {
+    return (
+        <div className='skills'>
+            <h1 className='skills__header'>Skills Section</h1>
+        </div>
+    )
+}
+
+export default Skills
